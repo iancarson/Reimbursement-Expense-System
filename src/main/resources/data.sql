7b26061635a1e5fad80007dcc199d6cf2dffffed
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-05 19:30:02 -0400

    Update data.sql
    Commented the unoptimized SQL commands.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- users
+/*-- users
 INSERT INTO user(id,username,first_name,last_name,password,email,address) VALUES (1,'acrenwelge','Andrew','Crenwelge', 'admin' ,'andrewcrenwelge@gmail.com', '11830 Sunrise Valley Dr Apt 545');
 INSERT INTO user(id,username,first_name,last_name,password,email,address) VALUES (2,'bcrenwelge','Blake' ,'Crenwelge', 'blake' ,'blakecrenwelge@gmail.com' , '1945 Blueridge Ave');
 INSERT INTO user(id,username,first_name,last_name,password,email,address) VALUES (3,'screnwelge','Seth'  ,'Crenwelge', 'seth'  ,'sethcrenwelge@gmail.com'  , '203 Sunridge Heights');
@@ -12,3 +12,4 @@ INSERT INTO reimbursement(user_id, is_approved) VALUES (1, true);
 INSERT INTO expense(reimbursement_id, amount, description) VALUES (1, 1.99, 'some description');
 INSERT INTO expense(reimbursement_id, amount, description) VALUES (1, 5.31, 'some description');
 INSERT INTO expense(reimbursement_id, amount, description) VALUES (1, 9.76, 'some description');
+*/
