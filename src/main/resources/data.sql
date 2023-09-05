26faefe5b515dc8674930330aaa0a79dcc03bd50
Ian Carson <38280903+iancarson@users.noreply.github.com>
2023-09-05 19:54:29 -0400

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,3 +20,32 @@ INSERT INTO expense(reimbursement_id, amount, description) VALUES (1, 9.76, 'som
 - Used a transaction to ensure that all related data is inserted consistently.
 - Used the lastval() function to retrieve the last inserted reimbursement_id and use it in the expense table inserts.
 */
+-- Start a transaction
+BEGIN;
+
+-- users (Batch insert)
+INSERT INTO user (id, username, first_name, last_name, password, email, address)
+VALUES
+  (1, 'acrenwelge', 'Andrew', 'Crenwelge', 'admin', 'andrewcrenwelge@gmail.com', '11830 Sunrise Valley Dr Apt 545'),
+  (2, 'bcrenwelge', 'Blake', 'Crenwelge', 'blake', 'blakecrenwelge@gmail.com', '1945 Blueridge Ave'),
+  (3, 'screnwelge', 'Seth', 'Crenwelge', 'seth', 'sethcrenwelge@gmail.com', '203 Sunridge Heights'),
+  (4, 'ncrenwelge', 'Nicole', 'Crenwelge', 'nicole', 'nicolecrenwelge@gmail.com', '11830 Sunrise Valley Dr Apt 545');
+
+-- reimbursements (Batch insert)
+INSERT INTO reimbursement (user_id, is_approved)
+VALUES
+  (1, false),
+  (1, true);
+
+-- Get the last inserted reimbursement_id
+SELECT lastval() INTO @last_reimbursement_id;
+
+-- expenses (Batch insert)
+INSERT INTO expense (reimbursement_id, amount, description)
+VALUES
+  (@last_reimbursement_id, 1.99, 'some description'),
+  (@last_reimbursement_id, 5.31, 'some description'),
+  (@last_reimbursement_id, 9.76, 'some description');
+
+-- Commit the transaction
+COMMIT;
